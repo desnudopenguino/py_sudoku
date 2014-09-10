e419b8d73c4b1f655729ef04d65af1f291793931
Bucky Townsend <desnudopenguino@gmail.com>
2014-09-10 16:00:47 -0700

    removed old files, and set it up so that the script will print a playable board

--- sudoku.py
+++ sudoku.py
@@ -1,63 +1,117 @@
-#This is a pythin sudoku board generator
+
+# this will generate a sudoku board
 import random
-#create the board
-def new_board(segment_size):
-	row_size = column_size = segment_size ** 2 # set length and width of board
-	#board array
+def generate_board(size):
+	full_size = size ** 2
 	board = []
-	# reset excludes and column count for each row
-	for row_index in range(row_size):
+	used_positions = [] # [x,y] coords for used positions
+	iterations = 0
+
+	#initialize board
+	for count in range(full_size):
 		board.append([])
-		used = []
-		for column_count in range(column_size):
-			new_number = generate_number(row_size, used)
-			used.append(new_number)
-			board[row_index].append(new_number)
+		for count2 in range(full_size):
+			board[count].append(0)
+	
+	#generate grid position
+	invalid_counter = 0
+	while check_board_full(used_positions) == False:
+		iterations += 1
+		pos = generate_position(full_size-1)
+		if check_used_position(pos, used_positions) == False:
+			#use other logic here to check if number is unique in row, col, & block
+			number = random.randint(1,full_size)
+			if check_row(number,pos[0],board) and check_column(number,pos[1],board) and check_block(number,pos[0],pos[1],board):
+				board[pos[0]][pos[1]] = number
+				used_positions.append(pos)
+			else:
+				invalid_counter += 1
+			
+			if invalid_counter == 50:
+				reset_positions(pos[0],pos[1],board,used_positions)
+				invalid_counter = 0
+				#reset nome numbers
+
+	print iterations
 	return board
 
-#makes sure that unique numbers are in both rows and cols
-def check_columns(board):
-	for row in range(len(board)):
-		for temp_row in range(len(board)):
-			if temp_row != row:
-				for col_index in range(9):
-					if board[row][col_index] == board[temp_row][col_index]:
-						board[temp_row].append(board[temp_row].pop(col_index))
-		
+def generate_position(end):
+	x = random.randint(0,end)
+	y = random.randint(0,end)
+	return [x,y]
 
-def generate_number(row_size, used):
-	new_number = random.randint(1,row_size)
-	while new_number in used:
-		new_number = random.randint(1,row_size)
-			
-	return new_number
-def print_board(board):
-	print "\n"
-	print "Sudoku Board!"
-	for row in board:
-		print row
 
-def generate_segment(row_size,used):
-	segment = []
-	count = 0
-	while count < row_size ** .5:
-		new_number = random.randint(1,row_size)
-		while new_number in used:
-			new_number = random.randint(1,row_size)
-		segment.append(new_number)
-		count += 1
-	return segment 
+def check_board_full(used_positions):
+	if len(used_positions) < 81:
+		return False
+	else:
+		return True
 
-def generate_block(row_size):
+def check_used_position(position, used_positions):
+	if position in used_positions:
+		return True
+	else:
+		return False
+
+def check_row(number, row_index, board):
+	row = []
+	for count in range(len(board)):
+		row.append(board[row_index][count])
+	if number in row:
+		return False
+	else:
+		return True
+
+def check_column(number, column_index, board):
+	column = []
+	for count in range(len(board)):
+		column.append(board[count][column_index])
+	if number in column:
+		return False
+	else:
+		return True
+
+def check_block(number, row_index, column_index, board):
+	block_x = row_index / 3
+	block_y = column_index / 3
 	block = []
-	used = []
-	for count in range(int(row_size ** .5)):
-		new_segment = generate_segment(row_size,block)
-		block.append(new_segment)
-		used += new_segment
-		print used
-		print block
 
-	return block
+	for row in range(block_x*3,block_x*3+3):
+		for column in range(block_y*3,block_y*3+3):
+			block.append(board[row][column])
+	
+	if number in block:
+		return False
+	else:
+		return True
 
-print generate_block(9)
+# if the system becomes locked up, reset some numbers to 0 to see if it can correct itself.
+def reset_positions(x_pos,y_pos,board,used_positions):
+	length = len(board)-1
+	x = random.randint(0,length)
+	y = random.randint(0,length)
+	while x == x_pos and y == y_pos:
+		x = random.randint(0,length)
+		y = random.randint(0,length)
+	
+	x_base = [x_pos,y]
+	y_base = [x,y_pos]
+
+	board[x_pos][y] = 0
+	board[x][y_pos] = 0
+
+	if x_base in used_positions:
+		used_positions.remove(x_base)
+	if y_base in used_positions:
+		used_positions.remove(y_base)
+
+
+def print_board(board):
+	print "Sudoku Solved!" 
+	for row in board:
+		for item in row:
+			print item ,
+		print "\b"
+
+board = generate_board(3)
+print_board(board)
